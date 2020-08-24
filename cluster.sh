6c49e010b7484ee4d371f0acd9b72eb23db515a7
typhainepl <typhaine@ebi.ac.uk>
2020-08-24 14:26:04 +0100

    improve clustering statistics

--- cluster.sh
+++ cluster.sh
@@ -2,7 +2,7 @@
 
 # @author T. Paysan-Lafosse
 # @brief this script concatains fasta files and generates MGnify/UniProt-KB clusters
-#usage: bsub -q production-rh74 -M 600000 -R "rusage[mem=600000]" -oo clustering_full_bidir.log -J cluster_full_uni -Pbigmem -n 16 ./cluster.sh clustering.cfg
+#usage: bsub -q production-rh74 -M 600000 -R "rusage[mem=600000]" -oo clustering_full_bidir.log -J cluster_full_bidir -Pbigmem -n 16 ./cluster.sh clustering.cfg
 
 if [[ $# -lt 1 ]]; then
     echo "Illegal number of parameters. Usage: cluster.sh config_file"
@@ -25,7 +25,8 @@ DATADIR="${SCRIPTDIR}/data"
 mkdir -p $DATADIR
 
 #get latest version of UniProtKB
-if [[ $UPDATE_UNIPROT -eq "yes" ]];then
+if [[ $UPDATE_UNIPROT == "yes" ]];then
+    echo "Updating UniProt"
     bsub -J "get_uniprotkb" -oo "${DATADIR}/uniprot.log" "${SCRIPTDIR}/update_uniprotkb.sh" $DATADIR
 fi
 
@@ -52,7 +53,9 @@ else
     exit
 fi
 
-bwait -w "ended(get_uniprotkb)"
+if [[ $UPDATE_UNIPROT == "yes" ]];then
+    bwait -w "ended(get_uniprotkb)"
+fi
 
 #concat MGnify and UniProtKB files
 UNIPROT_VERSION=`cut -d ' ' -f3 <(head -1 "${DATADIR}/relnotes.txt")`
@@ -60,8 +63,9 @@ UNIPROT_FASTA="${DATADIR}/uniprotkb_${UNIPROT_VERSION}.fasta"
 MGYUNI_FASTA="${DATADIR}/mgy_${MGNIFY_VERSION}_uniprot_${UNIPROT_VERSION}_full.fa"
 
 if [[ -s $MGYUNI_FASTA ]]; then
-    echo "${MGYUNI_FASTA} already exists, starting clustering"
+    echo "${MGYUNI_FASTA} already exists"
 else
+    echo "Cat Mgnify and UniProt files"
     `cat $UNIPROT_FASTA $MGNIFY_FASTA_FULL > $MGYUNI_FASTA `    
 fi
 
@@ -75,6 +79,7 @@ DB="${SUBDIR}/mgy_seqs"
 cluster_file="${DB}.cluster_seq.fa"
 
 if [[ ! -s $cluster_file ]];then
+    echo "Starting clustering"
     $mmseqs createdb $MGYUNI_FASTA $DB.mmseqs
     $mmseqs linclust $DB.mmseqs $DB.cluster "${SUBDIR}/tmp/" --min-seq-id 0.5 -c 0.75 --cov-mode 0 --threads 16
     $mmseqs result2repseq $DB.mmseqs $DB.cluster $DB.cluster_rep
@@ -82,22 +87,29 @@ if [[ ! -s $cluster_file ]];then
     $mmseqs createtsv $DB.mmseqs $DB.mmseqs $DB.cluster $DB.cluster.tsv
     $mmseqs createseqfiledb $DB.mmseqs $DB.cluster $DB.cluster_seq
     $mmseqs result2flat $DB.mmseqs $DB.mmseqs $DB.cluster_seq $DB.cluster_seq.fa
+    echo "Clustering completed successfully"
 fi
+
+list_accessions="${cluster_file}_percent_mgnify_2+_no_pfam"
 
 if [[ -s $cluster_file ]];then
     #counting cluster size and Mgnify percentages per cluster
     source ~oracle/ora112setup.sh
     prot_not_in_pfam="${DATADIR}/proteins_not_in_pfam_${UNIPROT_VERSION}.txt"
-    python3 "${SCRIPTDIR}/get_stats.py" -i $cluster_file -f $prot_not_in_pfam -u $USERNAME -p $PASSWORD -s $SCHEMA
+    echo "Deleting old clusters files"
+    rm -r "${SUBDIR}/clusters"
+    rm $list_accessions
+    python "${SCRIPTDIR}/get_stats.py" -i $cluster_file -f $prot_not_in_pfam -u $USERNAME -p $PASSWORD -s $SCHEMA
 else
     echo "Clustering failed"
     exit
 fi
 
-list_accessions="${cluster_file}_percent_mgnify_2+_no_pfam"
+list_accessions_sorted="${list_accessions}_sorted"
 if [[ -s $list_accessions ]];then
+    sort -r -k2 -n $list_accessions > $list_accessions_sorted
     #generate alignments for clusters with representative seq not found in Pfam
-    "${SCRIPTDIR}/generate_alignments_no_pfam.sh" $list_accessions
+    # python "${SCRIPTDIR}/generate_alignments_no_pfam.py" $list_accessions_sorted
 else
     echo "Getting statistics failed or no cluster found with more than 2 sequences and containing a UniProt sequence not in Pfam"
 fi
--- get_stats.py
+++ get_stats.py
@@ -6,8 +6,8 @@
 @brief This script split the clusters into multiple files 
         and counts the number of sequences per cluster and the percentage of MGnify and SwissProt sequences
 
-@arguments [-i input_file]: file countaining clustered sequences (fasta)
-            [-f protein_file]: file countaining the list of proteins not found in the Pfam database (can be empty or non existing)
+@arguments [-i inputfile]: file countaining clustered sequences (fasta)
+            [-f proteinfile]: file countaining the list of proteins not found in the Pfam database (can be empty or non existing)
             [-u username]: username for database connection
             [-p password]: password for database connection
             [-s schema]: schema for database connection (VIPREAD)
@@ -25,11 +25,10 @@ import itertools
 
 
 class process_cluster:
-    def __init__(self):
+    def __init__(self, inputfile):
         self.protein_dict = {}
-        self.dirname = ""
-        self.clusterdir = ""
-        self.count = 0
+        self.dirname = inputfile
+        self.clusterdir = os.path.join(inputfile, "clusters")
 
     def getConnection(self, user, password, schema):
         """
@@ -58,7 +57,7 @@ class process_cluster:
 
         return connection, cursor
 
-    def get_proteins_not_in_pfam(self, user, password, schema, protein_file):
+    def get_proteins_not_in_pfam(self, user, password, schema, proteinfile):
         connection, cursor = self.getConnection(user, password, schema)
 
         print("Searching for proteins not matching Pfam in the database")
@@ -70,7 +69,7 @@ class process_cluster:
         cursor.execute(sql)
         self.protein_dict = [row[0] for row in cursor]
         # print(protein_dict)
-        with open(protein_file, "w") as f:
+        with open(proteinfile, "w") as f:
             for protein in self.protein_dict:
                 f.write(f"{str(protein)}\n")
 
@@ -90,71 +89,70 @@ class process_cluster:
         rep = ""
         counting = ""
 
-        if self.count < 10000:  # end search if 10K clusters found
-            for item in chunk:
-                skip = False
-                if re.match(r"^>", item):
-                    if re.match(r">sp", item):  # seq from SwissProt
-                        countswiss += 1
-                        acc = item.split("|")[1]
-                    elif re.match(r">MGY", item):  # seq from MGnify
-                        countmgy += 1
-                        acc = item.split(">")[1].split(" ")[0]
-                        skip = True
-                    else:  # seq from TrEMBL
-                        acc = item.split("|")[1]
+        for item in chunk:
+            skip = False
+            if re.match(r"^>", item):
+                if re.match(r">sp", item):  # seq from SwissProt
+                    countswiss += 1
+                    acc = item.split("|")[1]
+                    item = item.replace("sp|", "")
+                elif re.match(r">MGY", item):  # seq from MGnify
+                    countmgy += 1
+                    acc = item.split(">")[1].split(" ")[0]
+                    skip = True
+                else:  # seq from TrEMBL
+                    acc = item.split("|")[1]
+                    item = item.replace("tr|", "")
 
-                    # print(acc)
+                # print(acc)
 
-                    if not skip and acc in self.protein_dict:
-                        inpfam += 1
-                        break
+                if not skip and acc not in self.protein_dict:
+                    inpfam += 1
+                    break
 
-                    # total number of seq
-                    count_total += 1
+                # total number of seq
+                count_total += 1
 
-                    # save line content to write in cluster files
-                    if count_total == 1:
-                        content += f">{acc}\n"
-                        rep = acc
+                # update rep to new representative accession
+                if count_total == 1:
+                    rep = acc
+                item = item.replace("|", " ")
+            content += item
 
-                content += item
+        # for clusters not found in Pfam and with at least 2 sequences
+        if inpfam == 0 and count_total > 1:
+            # print(f"Cluster {rep} not in Pfam")
 
-            # for clusters not found in Pfam and with at least 2 sequences
-            if inpfam == 0 and count_total > 1:
-                # print(f"Cluster {rep} not in Pfam")
+            # generate % MGnify sequences found in cluster
+            percentmgy = round(countmgy * 100 / count_total, 2)
+            # generate % SwissProt sequences found in cluster
+            percentswiss = round(countswiss * 100 / count_total, 2)
 
-                # generate % MGnify sequences found in cluster
-                percentmgy = round(countmgy * 100 / count_total, 2)
-                # generate % SwissProt sequences found in cluster
-                percentswiss = round(countswiss * 100 / count_total, 2)
+            # create sub directories to save clusters files if at least 1 UniProt seq and 1 mgnify seq
+            if percentmgy < 100.0 and percentmgy > 0.0:
+                if rep[0:3] == "MGY":
+                    subdir = os.path.join(self.clusterdir, rep[0:8])
+                else:
+                    subdir = os.path.join(self.clusterdir, rep[0:3])
+                os.makedirs(subdir, exist_ok=True)
 
-                # create sub directories to save clusters files if at least 1 UniProt seq and 1 mgnify seq
-                if percentmgy < 100.0 and percentmgy > 0.0:
-                    if rep[0:3] == "MGY":
-                        subdir = os.path.join(self.clusterdir, rep[0:8])
-                    else:
-                        subdir = os.path.join(self.clusterdir, rep[0:3])
-                    os.makedirs(subdir, exist_ok=True)
+                # save clusters in different files
+                with open(os.path.join(subdir, f"{rep}.fa"), "w") as clusterf:
+                    clusterf.write(content)
 
-                    # save clusters in different files
-                    with open(os.path.join(subdir, f"{rep}.fa"), "w") as clusterf:
-                        clusterf.write(content)
-
-                    counting = f"{rep}\t{count_total}\t{percentmgy}\t{percentswiss}\n"
-                    self.count += 1
-                    return counting
+                counting = f"{rep}\t{count_total}\t{percentmgy}\t{percentswiss}\n"
+                return counting
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", "--input_file", help="file countaining cluster_sequences (fasta)", required=True
+        "-i", "--inputfile", help="file countaining cluster_sequences (fasta)", required=True
     )
     parser.add_argument(
         "-f",
-        "--protein_file",
+        "--proteinfile",
         help="file countaining protein accessions not found in Pfam",
         required=True,
     )
@@ -163,44 +161,45 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--schema", help="database schema to connect to", required=True)
     args = parser.parse_args()
 
-    pc = process_cluster()
-
-    # get list of UniProt accessions not found in Pfam
-    if not os.path.isfile(args.protein_file) or os.path.getsize(args.protein_file) == 0:
-        pc.get_proteins_not_in_pfam(args.user, args.password, args.schema, args.protein_file)
-    else:
-        print("Loading proteins accessions into memory")
-        with open(args.protein_file, "r") as f:
-            pc.protein_dict = {line.strip("\n"): 1 for line in f}
-    # print(f"protein_dict: {len(pc.protein_dict)}")
-
-    pc.dirname = os.path.dirname(args.input_file)
-    pc.clusterdir = os.path.join(pc.dirname, "clusters")
+    pc = process_cluster(os.path.dirname(args.inputfile))
     os.makedirs(pc.clusterdir, exist_ok=True)
 
-    # protein_dict = []
+    # get list of UniProt accessions not found in Pfam
+    if not os.path.isfile(args.proteinfile) or os.path.getsize(args.proteinfile) == 0:
+        pc.get_proteins_not_in_pfam(args.user, args.password, args.schema, args.proteinfile)
+    else:
+        print("Loading proteins accessions into memory")
+        with open(args.proteinfile, "r") as f:
+            pc.protein_dict = {line.strip("\n"): 1 for line in f}
+
     print("Getting clusters' statistics")
 
     num_chunks = 5
     results = []
+    counter = 0
 
-    if os.path.isfile(args.input_file):
-        with open(args.input_file, "r") as inputf:
+    if os.path.isfile(args.inputfile):
+        with open(args.inputfile, "r") as inputf, open(
+            f"{args.inputfile}_percent_mgnify_2+_no_pfam", "w"
+        ) as output:
             chunks = itertools.groupby(inputf, pc.cluster_separator)
-            while True:
-                groups = []
-                for key, chunk in itertools.islice(chunks, num_chunks):
-                    if not key:
-                        groups.append(list(chunk))
-                if groups:
-                    with Pool(8) as pool:
-                        result = pool.map(pc.process, groups)
-                        results.extend(result)
-                else:
-                    break
-
-        with open(f"{args.input_file}_percent_mgnify_2+_no_pfam", "w") as output:
-            for cluster in results:
-                if cluster != None:
-                    output.write(cluster)
+            try:
+                while True:
+                    groups = []
+                    for key, chunk in itertools.islice(chunks, num_chunks):
+                        if not key:
+                            groups.append(list(chunk))
+                    if groups:
+                        with Pool(5) as pool:
+                            for cluster in pool.imap(pc.process, groups):
+                                if cluster != None:
+                                    counter += 1
+                                    output.write(cluster)
+                                # end search if 10K clusters found
+                                if counter >= 10000:
+                                    raise ValueError("Stop!!!")
+                    else:
+                        break
+            except ValueError:
+                print(f"Clustering check done, {counter} clusters saved")
 
